757b89ccf2d80ca5fe89ab38c168414440bf80da
AdosBorodoS <91018140+AdosBorodoS@users.noreply.github.com>
2021-10-02 21:57:37 +0300

    Update 03_hw.py

--- Module3/home_work/03_hw.py
+++ Module3/home_work/03_hw.py
@@ -2,3 +2,19 @@
 # Вывести на экран сумму всех положительных элементов кратных двум.
 
 # TODO: your code here
+import random
+numbers = []
+n = int(input("Введите количество элеменов в массиве: "))
+i = 0
+
+while i <= n-1:
+    numbers.append(random.randint(-100,100))
+    print(numbers[i])
+    i += 1
+
+print("-------------------")
+i1 = 0
+while i1 <= n-1:
+    if numbers[i1] >= 0 and numbers[i1] % 2 == 0:
+        print(numbers[i1])
+    i1 += 1
