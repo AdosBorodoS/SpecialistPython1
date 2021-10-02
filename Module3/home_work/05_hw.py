134a2ee60937e49a78a4e353c915eff80eda8460
AdosBorodoS <91018140+AdosBorodoS@users.noreply.github.com>
2021-10-02 21:58:38 +0300

    Update 05_hw.py

--- Module3/home_work/05_hw.py
+++ Module3/home_work/05_hw.py
@@ -4,3 +4,15 @@
 names = ["Иван", "Ирина", "Вячеслав", "Василий", "Петр"]
 
 # TODO: your code here
+
+index_max_len=" "
+i=0
+len_mas=[]
+
+while i<=len(names)-1:
+    len_mas.append(len(names[i]))
+    i+=1
+
+index_max_len=len_mas.index(max(len_mas))
+
+print(names[index_max_len])
